ac116991f9586654f92de89e457b03e9ac72b1a7
= <matierrez20gmail.com>
2024-09-25 11:34:01 -0300

    first commit

---
+++ data/usuarioData.cs
@@ -0,0 +1,20 @@
+﻿using apiPrueba.modelo;
+
+namespace apiPrueba.data
+{
+
+
+    public class usuarioData
+    {
+
+        public usuarioModel ObtieneUsuario(int id)
+        {
+
+
+            var data = new usuarioModel();
+             return data;
+
+        }
+
+    }
+}
---
+++ modelo/usuarioModel.cs
@@ -0,0 +1,11 @@
+﻿namespace apiPrueba.modelo
+{
+    public class usuarioModel
+    {
+
+        public string nmombre { get; set;  }
+        public string descripcion { get; set; }
+        public string categoria { get; set; }
+        public string precio { get; set; }
+    }
+}
